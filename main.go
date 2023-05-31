0d82122a83b33ae9ee121f10dcab6f4f063143ef
satowo619 <satodor1001@gmail.com>
2023-05-31 16:21:09 +0900

    update .env

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/oklog/ulid/v2"
 	"log"
 	"net/http"
@@ -29,25 +30,37 @@ type UserResForHTTPPost struct {
 var db *sql.DB
 
 func init() {
-	// ①-1
-	// DB接続のための準備,ローカルで実行するとき用
+	// DB接続のため環境変数を取得(本番環境用）
+	/*
+		mysqlUser := os.Getenv("MYSQL_USER")
+		mysqlPwd := os.Getenv("MYSQL_PASSWORD")
+		mysqlHost := os.Getenv("MYSQL_HOST")
+		mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	*/
+
+	// DB接続のため環境変数を取得（開発環境用）
+	err := godotenv.Load("../database/.env")
+	if err != nil {
+		fmt.Printf("読み込みできませんでした：%v", err)
+	}
+
 	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlPwd := os.Getenv("MYSQL_PASSWORD")
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
-	fmt.Println(mysqlUser)
 
-	//err := godotenv.Load(".env")
-	//if err != nil {
-	//	fmt.Printf("読み込みできませんでした：%v", err)
-	//}
+	fmt.Println("user:", mysqlUser)
+	fmt.Println("password:", mysqlPwd)
+	fmt.Println("database:", mysqlDatabase)
 
-	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	//本番環境用
+	//connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	//開発環境用
+	connStr := fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlPwd, mysqlDatabase)
 	_db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
-	// ①-3
+	//
 	if err := _db.Ping(); err != nil {
 		log.Fatalf("fail: _db.Ping, %v\n", err)
 	}
