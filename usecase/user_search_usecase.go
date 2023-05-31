4cb418d47af875a1ee250b220b82f857786432b1
satowo619 <satodor1001@gmail.com>
2023-05-31 17:28:08 +0900

    divide hundler/get

--- usecase/user_search_usecase.go
+++ usecase/user_search_usecase.go
@@ -1,1 +1,11 @@
-package controller
+package usecase
+
+import (
+	"hackathon-backend/dao"
+	"hackathon-backend/model"
+)
+
+func UserSearchUseCase() ([]model.User, error) {
+	users, err := dao.UserSearchDao()
+	return users, err
+}
