60bd279a6bffd683e7a69102004a0e9f2c5f8f05
satowo619 <satodor1001@gmail.com>
2023-05-31 18:38:20 +0900

    divide handler/post

--- controller/user_register_controller.go
+++ controller/user_register_controller.go
@@ -1,1 +1,62 @@
 package controller
+
+import (
+	"encoding/json"
+	"hackathon-backend/usecase"
+	"log"
+	"net/http"
+	"unicode/utf8"
+)
+
+type UserResForHTTPPost struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func UserRegisterController(w http.ResponseWriter, r *http.Request) {
+	// リクエストのボディを読み込み
+	var data UserResForHTTPPost
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		log.Printf("fail: json.NewDecoder(r.Body).Decode, %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//Nameが空文字か50字以上、Ageが81歳以上19歳以下の時エラーコード400を返す
+	if data.Name == "" || utf8.RuneCountInString(data.Name) > 50 || data.Age < 20 || data.Age > 80 {
+		log.Printf("fail: invalidinput, %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//受け取ったデータをキーごとに取り出す
+	name := data.Name
+	age := data.Age
+
+	// データをデータベースに挿入しそれを含んだ全userのnameとageを返す
+	users, err := usecase.UserRegisterUseCase(name, age)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	////レスポンス用のUserResForHTTPPostのリスト型に変換
+	usersRes := make([]UserResForHTTPPost, 0)
+	for _, u := range users {
+		usersRes = append(usersRes, UserResForHTTPPost{
+			Name: u.Name,
+			Age:  u.Age,
+		})
+	}
+
+	// 変換したユーザーデータリストをjson形式に変換
+	jsonResp, err := json.Marshal(usersRes)
+	if err != nil {
+		log.Printf("fail: json.Marshal, %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResp)
+}
--- dao/user_dao.go
+++ dao/user_dao.go
@@ -29,6 +29,45 @@ func UserSearchDao() ([]model.User, error) {
 	return users, nil
 }
 
+func UserRegisterDao(id string, name string, age int) ([]model.User, error) {
+	// データをデータベースに挿入
+	stmt, err := db.Prepare("INSERT INTO user (id, name, age) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Printf("fail: db.Prepare, %v\n", err)
+		return nil, err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id, name, age)
+	if err != nil {
+		log.Printf("fail: stmt.Exec, %v\n", err)
+		return nil, err
+	}
+
+	//レスポンスとして挿入したデータを含めた全データを返す
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		log.Printf("fail: db.Query, %v\n", err)
+		return nil, err
+	}
+
+	users := make([]model.User, 0)
+	for rows.Next() {
+		var u model.User
+		if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
+			log.Printf("fail: rows.Scan, %v\n", err)
+
+			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+				log.Printf("fail: rows.Close(), %v\n", err)
+			}
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
+}
+
 func DataBaseClose() error {
 	//DBを閉じる
 	err := db.Close()
