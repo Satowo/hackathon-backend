f9f3707ce04abb172c82842ea7db287e7e9362db
satowo619 <satodor1001@gmail.com>
2023-06-05 16:54:33 +0900

    add get-channel-messages

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 func headerSet(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	/*w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-frontend-eight.vercel.app")*/
+	/*w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")*/
+	w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-frontend-eight.vercel.app")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
