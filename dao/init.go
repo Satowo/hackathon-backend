580a2bf155fd5d62de339c328fa8c9762b71431c
satowo619 <satodor1001@gmail.com>
2023-10-29 13:53:29 +0900

    modify init.go

--- dao/init.go
+++ dao/init.go
@@ -5,36 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 func init() {
 	// DB接続のため環境変数を取得(本番環境用）
-	mysqlUser := os.Getenv("MYSQL_USER")
+	/* mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlPwd := os.Getenv("MYSQL_PWD")
 	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE") */
 
 	// DB接続のため環境変数を取得（開発環境用）
-	/*err := godotenv.Load("../database/.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("読み込みできませんでした：%v", err)
-	}
+	} 
 
 	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PASSWORD")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")*/
+	mysqlPwd := os.Getenv("MYSQL_PASSWORD") 
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
 	fmt.Println("user:", mysqlUser)
 	fmt.Println("password:", mysqlPwd)
 	fmt.Println("host:", mysqlHost)
 	fmt.Println("database:", mysqlDatabase)
 
-	//本番環境用
 	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	//開発環境用
-	/*connStr := fmt.Sprintf("%s:%s@(localhost:3308)/%s", mysqlUser, mysqlPwd, mysqlDatabase)*/
 	_db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
